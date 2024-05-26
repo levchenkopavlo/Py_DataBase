988fc8e1ff9ba5f03021f09ef279cf3ea08bb5a0
levchenkopavlo <levchenkopavlo@gmail.com>
2024-05-26 15:59:19 +0300

    pr4-2 DataBase commit1

--- hw4_create table.sql
+++ hw4_create table.sql
@@ -1,304 +1,314 @@
-CREATE TABLE Faculties (
-    Id SERIAL PRIMARY KEY,
-    Financing DECIMAL (10, 2) NOT NULL CHECK (Financing >= 0) DEFAULT 0,
-	Name VARCHAR(100) NOT NULL CHECK (Name != '') UNIQUE 
-	
-);
+-- CREATE TABLE Faculties (
+--     Id SERIAL PRIMARY KEY,
+--     Financing DECIMAL (10, 2) NOT NULL CHECK (Financing >= 0) DEFAULT 0,
+-- 	Name VARCHAR(100) NOT NULL CHECK (Name != '') UNIQUE 
+-- );
 
-INSERT INTO Faculties (Name)
-VALUES 
-    ('Faculty of Computer Science'),
-    ('Faculty of Mathematics'),
-    ('Faculty of Physics'),
-    ('Faculty of Chemistry'),
-    ('Faculty of Biology'),
-    ('Faculty of History'),
-    ('Faculty of Geography'),
-    ('Faculty of Economics'),
-    ('Faculty of Literature and Philosophy'),
-    ('Faculty of Psychology and Sociology'),
-    ('Faculty of Political Science and Law'),
-    ('Faculty of Engineering'),
-    ('Faculty of Architecture'),
-    ('Faculty of Business Administration');
+-- ALTER TABLE Faculties
+-- ADD COLUMN Dean VARCHAR(255);
 
-
-CREATE TABLE Departments (
-	Id SERIAL PRIMARY KEY,
-	Building INT NOT NULL CHECK (Building BETWEEN 1 AND 5),
-	Financing NUMERIC NOT NULL CHECK (Financing >= 0) DEFAULT 0,
-	Name VARCHAR(100) NOT NULL UNIQUE,
-	FacultyId INT NOT NULL,
-	FOREIGN KEY (FacultyId) REFERENCES Faculties(Id)
-);
-
-INSERT INTO Departments (Building, Financing, Name, FacultyId)
-VALUES 
-    (1, 10000, 'Department of Computer Science', 1),
-    (1, 15000, 'Department of Mathematics', 2),
-    (1, 12000, 'Department of Physics', 3),
-    (2, 8000, 'Department of Chemistry', 4),
-    (2, 9000, 'Department of Biology', 5),
-    (3, 11000, 'Department of History', 6),
-    (3, 7000, 'Department of Geography', 7),
-    (3, 13000, 'Department of Economics', 8),
-    (3, 14000, 'Department of Literature',9),
-    (3, 9500, 'Department of Philosophy',9),
-    (4, 10000, 'Department of Psychology', 10),
-    (4, 10500, 'Department of Sociology', 10);
+-- INSERT INTO Faculties (Name)
+-- VALUES 
+--     ('Faculty of Computer Science'),
+--     ('Faculty of Mathematics'),
+--     ('Faculty of Physics'),
+--     ('Faculty of Chemistry'),
+--     ('Faculty of Biology'),
+--     ('Faculty of History'),
+--     ('Faculty of Geography'),
+--     ('Faculty of Economics'),
+--     ('Faculty of Literature and Philosophy'),
+--     ('Faculty of Psychology and Sociology'),
+--     ('Faculty of Political Science and Law'),
+--     ('Faculty of Engineering'),
+--     ('Faculty of Architecture'),
+--     ('Faculty of Business Administration');
 
 
+-- CREATE TABLE Departments (
+-- 	Id SERIAL PRIMARY KEY,
+-- 	Building INT NOT NULL CHECK (Building BETWEEN 1 AND 5),
+-- 	Financing NUMERIC NOT NULL CHECK (Financing >= 0) DEFAULT 0,
+-- 	Name VARCHAR(100) NOT NULL UNIQUE,
+-- 	FacultyId INT NOT NULL,
+-- 	FOREIGN KEY (FacultyId) REFERENCES Faculties(Id)
+-- );
 
-CREATE TABLE Groups (
-    Id SERIAL PRIMARY KEY,
-    Name VARCHAR(10) NOT NULL UNIQUE,
-    Rating INT NOT NULL CHECK (Rating BETWEEN 0 AND 5),
-    Year INT NOT NULL CHECK (Year BETWEEN 1 AND 5),
-	DepartmentId INT NOT NULL,
-	FOREIGN KEY (DepartmentId) REFERENCES Groups(Id)
-);
-
-INSERT INTO Groups (DepartmentId, Name, Rating, Year)
-VALUES 
-    (1, 'Group01', 5, 1),
-    (2, 'Group02', 4, 1),
-    (3, 'Group03', 3, 1),
-    (4, 'Group04', 2, 1),
-    (5, 'P107', 1, 1),
-    (1, 'Group06', 5, 2),
-    (6, 'Group07', 4, 2),
-    (7, 'Group08', 3, 2),
-    (8, 'Group09', 2, 2),
-    (9, 'Group10', 1, 2),
-    (1, 'Group11', 5, 3),
-    (2,'Group12', 4, 3),
-    (10,'Group13', 3, 3),
-    (11,'Group14', 2, 3),
-    (4,'Group15', 1, 3),
-    (1, 'Group16', 5, 4),
-    (3, 'Group17', 4, 4),
-    (5, 'Group18', 3, 4),
-    (12, 'Group19', 2, 4),
-    (6, 'Group20', 1, 4),
-    (2, 'Group21', 5, 5),
-    (4, 'Group22', 4, 5),
-    (6, 'Group23', 3, 5),
-    (7, 'Group24', 2, 5),
-    (1, 'Group25', 1, 5),
-    (8, 'Group26', 3, 1),
-    (9, 'Group27', 2, 2),
-    (9, 'Group28', 4, 3),
-    (10, 'Group29', 5, 4),
-    (12, 'Group30', 2, 1),
-    (11, 'Group31', 3, 2),
-    (10, 'Group32', 4, 3);
-
-CREATE TABLE Curators (
-    Id SERIAL PRIMARY KEY,
-    Name VARCHAR NOT NULL,
-	Surname VARCHAR NOT NULL);
-
-INSERT INTO Curators (Name, Surname)
-VALUES 
-    ('John', 'Doe'),
-    ('Jane', 'Smith'),
-    ('Emily', 'Johnson'),
-    ('Michael', 'Williams'),
-    ('Sarah', 'Brown'),
-    ('David', 'Jones'),
-    ('Laura', 'Garcia'),
-    ('Robert', 'Martinez'),
-    ('Maria', 'Rodriguez'),
-    ('James', 'Davis');
-
-CREATE TABLE GroupsCurators (
-	Id SERIAL PRIMARY KEY,
-	CuratorId INT NOT NULL,
-	GroupId INT NOT NULL,
-	FOREIGN KEY (CuratorId) REFERENCES Curators(Id),
-	FOREIGN KEY (GroupId) REFERENCES Groups(Id)
-);
-
-INSERT INTO GroupsCurators (CuratorId, GroupId)
-VALUES 
-    (1, 1),
-    (1, 2),
-	(1, 3),
-	(2, 4),
-    (2, 5),
-	(2, 6),
-	(3, 7),
-    (3, 8),
-	(3, 9),
-	(4, 10),
-    (4, 11),
-	(4, 12),
-	(5, 13),
-    (5, 14),
-	(5, 15),
-	(6, 16),
-    (6, 17),
-	(6, 18),
-	(7, 19),
-	(7, 20),
-	(7, 21),
-	(8, 22),
-	(8, 23),
-	(8, 24),
-	(9, 25),
-	(9, 26),
-	(10, 27),
-	(1, 28),
-	(1, 29),
-	(2, 30),
-	(3, 31),
-	(5, 32);
-
-CREATE TABLE Subjects (
-	Id SERIAL PRIMARY KEY,
-	Name VARCHAR(100) NOT NULL
-);
-
-INSERT INTO Subjects (Name)
-VALUES 
-    ('Mathematics'),
-    ('Physics'),
-    ('Chemistry'),
-    ('Biology'),
-    ('History'),
-    ('Geography'),
-    ('Economics'),
-    ('Literature'),
-    ('Philosophy'),
-    ('Psychology'),
-    ('Sociology'),
-    ('Computer Science'),
-    ('Anthropology'),
-    ('Archeology'),
-    ('Art History'),
-    ('Astronomy'),
-    ('Astrophysics'),
-    ('Botany'),
-    ('Database Theory'),
-    ('Drama'),
-    ('Ecology'),
-    ('Engineering'),
-    ('English'),
-    ('Ethnic Studies'),
-    ('Film Studies'),
-    ('Gender Studies'),
-    ('Health Science'),
-    ('Journalism'),
-    ('Law'),
-    ('Linguistics'),
-    ('Marketing'),
-    ('Music'),
-    ('Nutrition'),
-    ('Political Science'),
-    ('Religious Studies'),
-    ('Sociology'),
-    ('Statistics'),
-    ('Theater'),
-    ('Urban Studies'),
-    ('Visual Arts'),
-    ('Zoology'),
-    ('Foreign Language'),
-    ('Physical Education'),
-    ('Environmental Science'),
-    ('Business Administration'),
-    ('Public Administration'),
-    ('Humanities'),
-    ('Management');
-
-
-CREATE TABLE Teachers (
-    Id SERIAL PRIMARY KEY,
-    IsProfessor BOOLEAN  NOT NULL DEFAULT false,
-    Name VARCHAR NOT NULL,
-    Surname VARCHAR NOT NULL,
-    Salary DECIMAL (10, 2) NOT NULL CHECK (Salary > 0)
-);
-
-INSERT INTO Teachers (IsProfessor, Name, Surname, Salary)
-VALUES 
-    (false, 'Samantha', 'Adams', 50000),
-    (false, 'Jane', 'Smith', 48000),
-    (true, 'Michael', 'Johnson', 75000),
-    (false, 'Emily', 'Williams', 52000),
-    (false, 'Sarah', 'Brown', 49000),
-    (true, 'David', 'Jones', 72000),
-    (false, 'Laura', 'Garcia', 51000),
-    (false, 'Robert', 'Martinez', 49000),
-    (true, 'Maria', 'Rodriguez', 73000),
-    (false, 'James', 'Davis', 50000),
-    (false, 'Linda', 'Miller', 49000),
-    (true, 'William', 'Wilson', 74000),
-    (false, 'Barbara', 'Moore', 50000),
-    (false, 'Richard', 'Taylor', 48000),
-    (true, 'Susan', 'Anderson', 71000),
-    (false, 'Joseph', 'Thomas', 51000),
-    (false, 'Patricia', 'Jackson', 50000),
-    (true, 'Charles', 'White', 72000),
-    (false, 'Christopher', 'Harris', 53000),
-    (false, 'Daniel', 'Clark', 50000),
-    (true, 'Amanda', 'Lewis', 70000),
-    (false, 'Karen', 'Lee', 54000),
-    (false, 'Steven', 'Walker', 51000),
-    (true, 'Deborah', 'Hall', 71000),
-    (false, 'Dorothy', 'Allen', 55000),
-    (false, 'Matthew', 'Young', 52000),
-    (true, 'Nancy', 'Hill', 73000),
-    (false, 'Mark', 'Wright', 56000),
-    (false, 'Lisa', 'Scott', 53000);
-
-CREATE TABLE Lectures (
-    Id SERIAL PRIMARY KEY,
-	LectureRoom VARCHAR NOT NULL CHECK (LectureRoom != ''),
-    Date DATE NOT NULL CHECK (Date <= CURRENT_DATE),
-    SubjectId INT NOT NULL,
-    TeacherId INT NOT NULL,
-    FOREIGN KEY (SubjectId) REFERENCES Subjects(Id),
-    FOREIGN KEY (TeacherId) REFERENCES Teachers(Id)
-);
-
-
-INSERT INTO Lectures (LectureRoom, Date, SubjectId, TeacherId) VALUES
-('B103', '2023-05-01', 1, 1),
-('Room 202', '2023-05-15', 2, 2),
-('Room 303', '2023-04-25', 3, 3),
-('Room 104', '2023-03-10', 4, 4),
-('Room 105', '2023-02-20', 5, 5),
-('Room 106', '2023-01-30', 6, 6),
-('Room 107', '2023-05-05', 1, 2),
-('Room 108', '2023-04-15', 2, 3),
-('Room 109', '2023-03-25', 3, 4),
-('Room 110', '2023-02-10', 4, 5),
-('Room 111', '2023-01-15', 5, 6),
-('B103', '2023-05-20', 6, 1);
+-- INSERT INTO Departments (Building, Financing, Name, FacultyId)
+-- VALUES 
+--     (1, 10000, 'Department of Computer Science', 1),
+--     (1, 15000, 'Department of Mathematics', 2),
+--     (1, 12000, 'Department of Physics', 3),
+--     (2, 8000, 'Department of Chemistry', 4),
+--     (2, 9000, 'Department of Biology', 5),
+--     (3, 11000, 'Department of History', 6),
+--     (3, 7000, 'Department of Geography', 7),
+--     (3, 13000, 'Department of Economics', 8),
+--     (3, 14000, 'Department of Literature',9),
+--     (3, 9500, 'Department of Philosophy',9),
+--     (4, 10000, 'Department of Psychology', 10),
+--     (4, 10500, 'Department of Sociology', 10);
 
 
 
-CREATE TABLE GroupsLectures(
-	Id SERIAL PRIMARY KEY,
-	GroupId INT NOT NULL,
-	LectureId INT NOT NULL,
-	FOREIGN KEY (GroupId) REFERENCES GROUPS(ID),
-	FOREIGN KEY (LectureId) REFERENCES LECTURES(ID)
-);
+-- CREATE TABLE Groups (
+--     Id SERIAL PRIMARY KEY,
+--     Name VARCHAR(10) NOT NULL UNIQUE,
+--     Rating INT NOT NULL CHECK (Rating BETWEEN 0 AND 5),
+--     Year INT NOT NULL CHECK (Year BETWEEN 1 AND 5),
+-- 	DepartmentId INT NOT NULL,
+-- 	FOREIGN KEY (DepartmentId) REFERENCES Groups(Id)
+-- );
 
-INSERT INTO GroupsLectures (GroupId, LectureId) VALUES
-(1, 1),
-(1, 2),
-(2, 3),
-(2, 4),
-(3, 5),
-(3, 6),
-(4, 7),
-(4, 8),
-(5, 9),
-(5, 10),
-(1, 11),
-(2, 12);
+-- INSERT INTO Groups (DepartmentId, Name, Rating, Year)
+-- VALUES 
+--     (1, 'Group01', 5, 1),
+--     (2, 'Group02', 4, 1),
+--     (3, 'Group03', 3, 1),
+--     (4, 'Group04', 2, 1),
+--     (5, 'P107', 1, 1),
+--     (1, 'Group06', 5, 2),
+--     (6, 'Group07', 4, 2),
+--     (7, 'Group08', 3, 2),
+--     (8, 'Group09', 2, 2),
+--     (9, 'Group10', 1, 2),
+--     (1, 'Group11', 5, 3),
+--     (2,'Group12', 4, 3),
+--     (10,'Group13', 3, 3),
+--     (11,'Group14', 2, 3),
+--     (4,'Group15', 1, 3),
+--     (1, 'Group16', 5, 4),
+--     (3, 'Group17', 4, 4),
+--     (5, 'Group18', 3, 4),
+--     (12, 'Group19', 2, 4),
+--     (6, 'Group20', 1, 4),
+--     (2, 'Group21', 5, 5),
+--     (4, 'Group22', 4, 5),
+--     (6, 'Group23', 3, 5),
+--     (7, 'Group24', 2, 5),
+--     (1, 'Group25', 1, 5),
+--     (8, 'Group26', 3, 1),
+--     (9, 'Group27', 2, 2),
+--     (9, 'Group28', 4, 3),
+--     (10, 'Group29', 5, 4),
+--     (12, 'Group30', 2, 1),
+--     (11, 'Group31', 3, 2),
+--     (10, 'Group32', 4, 3);
+
+-- CREATE TABLE Curators (
+--     Id SERIAL PRIMARY KEY,
+--     Name VARCHAR NOT NULL,
+-- 	Surname VARCHAR NOT NULL);
+
+-- INSERT INTO Curators (Name, Surname)
+-- VALUES 
+--     ('John', 'Doe'),
+--     ('Jane', 'Smith'),
+--     ('Emily', 'Johnson'),
+--     ('Michael', 'Williams'),
+--     ('Sarah', 'Brown'),
+--     ('David', 'Jones'),
+--     ('Laura', 'Garcia'),
+--     ('Robert', 'Martinez'),
+--     ('Maria', 'Rodriguez'),
+--     ('James', 'Davis');
+
+-- CREATE TABLE GroupsCurators (
+-- 	Id SERIAL PRIMARY KEY,
+-- 	CuratorId INT NOT NULL,
+-- 	GroupId INT NOT NULL,
+-- 	FOREIGN KEY (CuratorId) REFERENCES Curators(Id),
+-- 	FOREIGN KEY (GroupId) REFERENCES Groups(Id)
+-- );
+
+-- INSERT INTO GroupsCurators (CuratorId, GroupId)
+-- VALUES 
+--     (1, 1),
+--     (1, 2),
+-- 	(1, 3),
+-- 	(2, 4),
+--     (2, 5),
+-- 	(2, 6),
+-- 	(3, 7),
+--     (3, 8),
+-- 	(3, 9),
+-- 	(4, 10),
+--     (4, 11),
+-- 	(4, 12),
+-- 	(5, 13),
+--     (5, 14),
+-- 	(5, 15),
+-- 	(6, 16),
+--     (6, 17),
+-- 	(6, 18),
+-- 	(7, 19),
+-- 	(7, 20),
+-- 	(7, 21),
+-- 	(8, 22),
+-- 	(8, 23),
+-- 	(8, 24),
+-- 	(9, 25),
+-- 	(9, 26),
+-- 	(10, 27),
+-- 	(1, 28),
+-- 	(1, 29),
+-- 	(2, 30),
+-- 	(3, 31),
+-- 	(5, 32);
+
+-- CREATE TABLE Subjects (
+-- 	Id SERIAL PRIMARY KEY,
+-- 	Name VARCHAR(100) NOT NULL
+-- );
+
+-- INSERT INTO Subjects (Name)
+-- VALUES 
+--     ('Mathematics'),
+--     ('Physics'),
+--     ('Chemistry'),
+--     ('Biology'),
+--     ('History'),
+--     ('Geography'),
+--     ('Economics'),
+--     ('Literature'),
+--     ('Philosophy'),
+--     ('Psychology'),
+--     ('Sociology'),
+--     ('Computer Science'),
+--     ('Anthropology'),
+--     ('Archeology'),
+--     ('Art History'),
+--     ('Astronomy'),
+--     ('Astrophysics'),
+--     ('Botany'),
+--     ('Database Theory'),
+--     ('Drama'),
+--     ('Ecology'),
+--     ('Engineering'),
+--     ('English'),
+--     ('Ethnic Studies'),
+--     ('Film Studies'),
+--     ('Gender Studies'),
+--     ('Health Science'),
+--     ('Journalism'),
+--     ('Law'),
+--     ('Linguistics'),
+--     ('Marketing'),
+--     ('Music'),
+--     ('Nutrition'),
+--     ('Political Science'),
+--     ('Religious Studies'),
+--     ('Sociology'),
+--     ('Statistics'),
+--     ('Theater'),
+--     ('Urban Studies'),
+--     ('Visual Arts'),
+--     ('Zoology'),
+--     ('Foreign Language'),
+--     ('Physical Education'),
+--     ('Environmental Science'),
+--     ('Business Administration'),
+--     ('Public Administration'),
+--     ('Humanities'),
+--     ('Management');
+
+
+-- CREATE TABLE Teachers (
+--     Id SERIAL PRIMARY KEY,
+--     IsProfessor BOOLEAN  NOT NULL DEFAULT false,
+--     Name VARCHAR NOT NULL,
+--     Surname VARCHAR NOT NULL,
+--     Salary DECIMAL (10, 2) NOT NULL CHECK (Salary > 0)
+-- );
+
+-- ALTER TABLE TEACHERS
+-- ADD COLUMN EmploymentDate DATE CHECK (EmploymentDate >= '1990-01-01');
+
+-- INSERT INTO Teachers (IsProfessor, Name, Surname, Salary)
+-- VALUES 
+--     (false, 'Samantha', 'Adams', 50000),
+--     (false, 'Jane', 'Smith', 48000),
+--     (true, 'Michael', 'Johnson', 75000),
+--     (false, 'Emily', 'Williams', 52000),
+--     (false, 'Sarah', 'Brown', 49000),
+--     (true, 'David', 'Jones', 72000),
+--     (false, 'Laura', 'Garcia', 51000),
+--     (false, 'Robert', 'Martinez', 49000),
+--     (true, 'Maria', 'Rodriguez', 73000),
+--     (false, 'James', 'Davis', 50000),
+--     (false, 'Linda', 'Miller', 49000),
+--     (true, 'William', 'Wilson', 74000),
+--     (false, 'Barbara', 'Moore', 50000),
+--     (false, 'Richard', 'Taylor', 48000),
+--     (true, 'Susan', 'Anderson', 71000),
+--     (false, 'Joseph', 'Thomas', 51000),
+--     (false, 'Patricia', 'Jackson', 50000),
+--     (true, 'Charles', 'White', 72000),
+--     (false, 'Christopher', 'Harris', 53000),
+--     (false, 'Daniel', 'Clark', 50000),
+--     (true, 'Amanda', 'Lewis', 70000),
+--     (false, 'Karen', 'Lee', 54000),
+--     (false, 'Steven', 'Walker', 51000),
+--     (true, 'Deborah', 'Hall', 71000),
+--     (false, 'Dorothy', 'Allen', 55000),
+--     (false, 'Matthew', 'Young', 52000),
+--     (true, 'Nancy', 'Hill', 73000),
+--     (false, 'Mark', 'Wright', 56000),
+--     (false, 'Lisa', 'Scott', 53000);
+
+-- ALTER TABLE TEACHERS
+-- ADD COLUMN IsAssistant BOOLEAN  NOT NULL DEFAULT false,
+-- ADD COLUMN Position VARCHAR NOT NULL DEFAULT '',
+-- ADD COLUMN Premium DECIMAL(10, 2) NOT NULL DEFAULT 0 CHECK (Premium >= 0);
+
+-- CREATE TABLE Lectures (
+--     Id SERIAL PRIMARY KEY,
+-- 	LectureRoom VARCHAR NOT NULL CHECK (LectureRoom != ''),
+--     Date DATE NOT NULL CHECK (Date <= CURRENT_DATE),
+--     SubjectId INT NOT NULL,
+--     TeacherId INT NOT NULL,
+--     FOREIGN KEY (SubjectId) REFERENCES Subjects(Id),
+--     FOREIGN KEY (TeacherId) REFERENCES Teachers(Id)
+-- );
+
+
+-- INSERT INTO Lectures (LectureRoom, Date, SubjectId, TeacherId) VALUES
+-- ('B103', '2023-05-01', 1, 1),
+-- ('Room 202', '2023-05-15', 2, 2),
+-- ('Room 303', '2023-04-25', 3, 3),
+-- ('Room 104', '2023-03-10', 4, 4),
+-- ('Room 105', '2023-02-20', 5, 5),
+-- ('Room 106', '2023-01-30', 6, 6),
+-- ('Room 107', '2023-05-05', 1, 2),
+-- ('Room 108', '2023-04-15', 2, 3),
+-- ('Room 109', '2023-03-25', 3, 4),
+-- ('Room 110', '2023-02-10', 4, 5),
+-- ('Room 111', '2023-01-15', 5, 6),
+-- ('B103', '2023-05-20', 6, 1);
+
+
+
+-- CREATE TABLE GroupsLectures(
+-- 	Id SERIAL PRIMARY KEY,
+-- 	GroupId INT NOT NULL,
+-- 	LectureId INT NOT NULL,
+-- 	FOREIGN KEY (GroupId) REFERENCES GROUPS(ID),
+-- 	FOREIGN KEY (LectureId) REFERENCES LECTURES(ID)
+-- );
+
+-- INSERT INTO GroupsLectures (GroupId, LectureId) VALUES
+-- (1, 1),
+-- (1, 2),
+-- (2, 3),
+-- (2, 4),
+-- (3, 5),
+-- (3, 6),
+-- (4, 7),
+-- (4, 8),
+-- (5, 9),
+-- (5, 10),
+-- (1, 11),
+-- (2, 12);
 
 
 -- CREATE TABLE Students (
--- hw4_queries_2.sql
+++ hw4_queries_2.sql
@@ -1,30 +1,77 @@
--- 1. Виведіть кількість викладачів кафедри «Software Development».
+-- 1. Вивести таблицю кафедр, але розташувати її поля у зворотному порядку.
+SELECT NAME
+FROM DEPARTMENTS
+ORDER BY Name DESC;
 
--- 2. Виведіть кількість лекцій, які веде викладач «Christopher Harris».
-SELECT COUNT(ID)
-FROM LECTURES
-WHERE TEACHERID = (
-	SELECT ID 
-	FROM TEACHERS
-	WHERE LOWER(NAME) = LOWER('Christopher') AND LOWER(SURNAME) = LOWER('Harris')
-);
+-- 2. Вивести назви груп та їх рейтинги з уточненнями до назв полів відповідно до назви таблиці.
+SELECT NAME AS "GROUP NAME", RATING AS "RATING"
+FROM GROUPS;
 
--- 3. Виведіть кількість занять, які проводяться в аудиторії «D201».
--- 4. Виведіть назви аудиторій та кількість лекцій, які проводяться в них.
--- 5. Виведіть кількість студентів, які відвідують лекції викладача «Jack Underhill».
+-- 3. Вивести для викладачів їх прізвища, відсоток ставки по
+-- відношенню до надбавки та відсоток ставки по відношенню до зарплати (сума ставки та надбавки).
+SELECT SURNAME,
+	ROUND(PREMIUM / SALARY * 100, 2) AS "FROM PREMIUM %",
+	ROUND((SALARY + PREMIUM) / SALARY *100, 2) AS "FROM TOTAL %"
+FROM TEACHERS
+ORDER BY SURNAME;
 
--- SELECT STUDENTS.ID, NAME, SURNAME, GROUPID
--- FROM STUDENTS
--- JOIN GROUPSSTUDENTS ON STUDENTS.ID = STUDENTID
--- WHERE GROUPSSTUDENTS.GROUPID = 
+-- 4. Вивести таблицю факультетів одним полем у такому форматі: «The dean of faculty [faculty] is [dean].».
+SELECT CONCAT('The dean of faculty ', Name, ' is ', Dean) AS "Faculty Information" 
+FROM FACULTIES;
 
+-- 5. Вивести прізвища професорів, ставка яких перевищує 1050.
+SELECT SURNAME
+FROM TEACHERS
+WHERE ISPROFESSOR IS TRUE AND SALARY > 1050;
 
+-- 6. Вивести назви кафедр, фонд фінансування яких менший, ніж 11000 або більший за 25000.
+SELECT NAME
+FROM DEPARTMENTS
+WHERE FINANCING NOT BETWEEN 11000 AND 25000;
 
--- 6. Виведіть середню ставку викладачів факультету «Computer Science».
--- 7. Виведіть мінімальну та максимальну кількість студентів серед усіх груп.
--- 8. Виведіть середній фонд фінансування кафедр.
--- 9. Виведіть повні імена викладачів і кількість проведених ними дисциплін.
--- 10. Виведіть кількість лекцій у кожен день тижня.
--- 11. Виведіть номери аудиторій та кількість кафедр, чиї лекції у них ведуться.
--- 12. Виведіть назви факультетів і кількість предметів, які на них ведуться.
--- 13. Виведіть кількість лекцій для кожної пари викладач-аудиторія.
+-- 7. Вивести назви факультетів, окрім факультету «Computer Science».
+SELECT NAME 
+FROM FACULTIES
+WHERE NAME NOT LIKE 'Computer Science';
+
+-- 8. Вивести прізвища та посади викладачів, які не є професорами.
+SELECT SURNAME, POSITION
+FROM TEACHERS
+WHERE NOT ISPROFESSOR;
+
+-- 9. Вивести прізвища, посади, ставки та надбавки асистентів, надбавка яких у діапазоні від 160 до 550.
+SELECT SURNAME, POSITION, SALARY, PREMIUM
+FROM TEACHERS
+WHERE ISASSISTANT AND PREMIUM BETWEEN 160 AND 550;
+
+-- 10. Вивести прізвища та ставки асистентів.
+SELECT SURNAME, SALARY
+FROM TEACHERS
+WHERE ISASSISTANT;
+
+-- 11. Вивести прізвища та посади викладачів, які були прийняті на роботу до 01.01.2000.
+SELECT SURNAME, POSITION
+FROM TEACHERS
+WHERE EmploymentDate <= '01-01-2000';
+
+-- 12. Вивести назви кафедр, які в алфавітному порядку розміщені до кафедри «Software Development». Виведене поле 
+-- назвіть «Name of Department».
+SELECT NAME AS "Name of Department"
+FROM DEPARTMENTS
+WHERE NAME < 'Software Development'
+ORDER BY NAME;
+
+-- 13. Вивести прізвища асистентів із зарплатою (сума ставки та надбавки) не більше 1200.
+SELECT SURNAME
+FROM TEACHERS
+WHERE ISASSISTANT AND SALARY + PREMIUM <= 1200;
+
+-- 14. Вивести назви груп 5-го курсу з рейтингом у діапазоні від 2 до 4.
+SELECT NAME 
+FROM GROUPS
+WHERE YEAR = 5 AND RATING BETWEEN 2 AND 4;
+
+-- 15. Вивести прізвища асистентів зі ставкою менше, ніж 550 або надбавкою менше, ніж 200
+SELECT SURNAME
+FROM TEACHERS
+WHERE ISASSISTANT AND (SALARY < 550 OR PREMIUM < 200);
